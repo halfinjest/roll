adcddd88b498270571c5216229ce33d8765869f3
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-04 12:32:05 -0600

    Created definitions for pips, rows, and configurations of columns and rows.

--- roll.c
+++ roll.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define CONF_COL "020103123404424444"
+#define CONF_ROW "000100010001101111"
 #define FOR_3(x) for (x = 0; x < 3; x++)
 #define FOR_l(x) for (x = 0; x < l; x++)
+#define PIP pipRow[3 * (ROW - '0') + k]
+#define ROW pipCol[3 * dice[j] + i]
 
 int main(int argc, char **argv)
 {
 	if (argc > 1 && atoi(argv[1]) > 0)
 	{
-		char *pipRow = "000100010001101111", *pipCol = "020103123404424444";
+		char *pipRow = CONF_ROW, *pipCol = CONF_COL;
 		int i, j, k, l = atoi(argv[1]), dice[l];
 		srand(time(NULL));
 		FOR_l(i) dice[i] = rand() % 6;
@@ -22,7 +26,7 @@ int main(int argc, char **argv)
 				printf("  |  ");
 				FOR_3(k)
 				{
-					if (pipRow[3 * (pipCol[3 * dice[j] + i] - '0') + k] - '0') printf("* ");
+					if (PIP - '0') printf("* ");
 					else printf("  ");
 				}
 				printf("  |");
