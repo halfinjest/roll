7ff3fe1954a70c17795a7580e6ac1a6071e23720
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-04 02:51:54 -0600

    Syntax changes.

--- roll.c
+++ roll.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define FOR_L for (i = 0; i < l; i++)
+#define FOR_L(x) for (x = 0; x < l; x++)
 
 int main(int argc, char **argv)
 {
@@ -9,14 +9,14 @@ int main(int argc, char **argv)
 		char *pipRow = "000100010001101111", *pipCol = "020103123404424444";
 		int i, j, k, l = atoi(argv[1]), dice[l];
 		srand(time(NULL));
-		FOR_L dice[i] = rand() % 6;
-		FOR_L printf("   __________ ");
+		FOR_L(i) dice[i] = rand() % 6;
+		FOR_L(i) printf("   __________ ");
 		printf("\n");
-		FOR_L printf("  |          |");
+		FOR_L(i) printf("  |          |");
 		printf("\n");
 		for (i = 0; i < 3; i++)
 		{
-			for (j = 0; j < l; j++)
+			FOR_L(j)
 			{
 				printf("  |  ");
 				for (k = 0; k < 3; k++)
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 			}
 			printf("\n");
 		}
-		FOR_L printf("  |__________|");
+		FOR_L(i) printf("  |__________|");
 		printf("\n\n");
 	}
 	else printf("Usage: roll <integer 1..>\n");
