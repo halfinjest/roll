c2da098abaf1933a3328bc64085d66fd2c0ca1fd
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-05 16:41:28 -0600

    Changed variables.

--- roll.h
+++ roll.h
@@ -1,11 +1,11 @@
-#define CONF_COL "020103123404424444"
-#define CONF_ROW "000100010001101111"
-#define INST_PIP pipRow[3 * (INST_ROW) + k] - '0'
-#define INST_ROW pipCol[3 * dice[j] + i] - '0'
+#define CONF_PIP "000100010001101111"
+#define CONF_ROW "020103123404424444"
+#define INST_PIP pips[3 * (INST_ROW) + k] - '0'
+#define INST_ROW rows[3 * dice[j] + i] - '0'
 
 void roll(int length, int *dice)
 {
-	char *pipCol = CONF_COL, *pipRow = CONF_ROW;
+	char *pips = CONF_PIP, *rows = CONF_ROW;
 	int i, j, k;
 	for (i = 0; i < length; i++) printf("   __________ ");
 	printf("\n");
