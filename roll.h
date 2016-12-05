e37035fd6a59191f787ddfd23610bec866de5ebe
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-05 16:08:31 -0600

    Removed definitions of loops.

--- roll.h
+++ roll.h
@@ -1,24 +1,22 @@
 #define CONF_COL "020103123404424444"
 #define CONF_ROW "000100010001101111"
-#define FOR_3(x) for (x = 0; x < 3; x++)
-#define FOR_l(x) for (x = 0; x < l; x++)
 #define INST_PIP pipRow[3 * (INST_ROW) + k] - '0'
 #define INST_ROW pipCol[3 * dice[j] + i] - '0'
 
 void roll(int length, int *dice)
 {
 	char *pipCol = CONF_COL, *pipRow = CONF_ROW;
-	int i, j, k, l = length;
-	FOR_l(i) printf("   __________ ");
+	int i, j, k;
+	for (i = 0; i < length; i++) printf("   __________ ");
 	printf("\n");
-	FOR_l(i) printf("  |          |");
+	for (i = 0; i < length; i++) printf("  |          |");
 	printf("\n");
-	FOR_3(i)
+	for (i = 0; i < 3; i++)
 	{
-		FOR_l(j)
+		for (j = 0; j < length; j++)
 		{
 			printf("  |  ");
-			FOR_3(k)
+			for (k = 0; k < 3; k++)
 			{
 				if (INST_PIP) printf("* ");
 				else printf("  ");
@@ -27,6 +25,6 @@ void roll(int length, int *dice)
 		}
 		printf("\n");
 	}
-	FOR_l(i) printf("  |__________|");
+	for (i = 0; i < length; i++) printf("  |__________|");
 	printf("\n\n");
 }
