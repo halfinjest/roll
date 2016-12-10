894b15a9f85b2830d02e30fe3c69e91148622ee4
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-10 17:48:38 -0600

    Variable changes.

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main(int argc, char **argv)
 {
 	if (argc > 1 && atoi(argv[1]) > 0)
 	{
-		int i, j = atoi(argv[1]), *dice = malloc(sizeof(int) * j);
+		int i, length = atoi(argv[1]), *dice = malloc(sizeof(int) * length);
 		srand(time(NULL));
-		for (i = 0; i < j; i++) dice[i] = rand() % 6;
-		roll(j, dice);
+		for (i = 0; i < length; i++) dice[i] = rand() % 6;
+		roll(length, dice);
 	}
 	else printf("Usage: roll <integer 1..>\n");
 	return 0;
